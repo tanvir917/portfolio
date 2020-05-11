fd561c98e830e2849c0efa76450b14c62b23ee22
tanvir <mdtanvirislams@gmail.com>
2020-05-11 15:22:41 +0600

    initializing setups

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        console.log(this);
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+        //bind allow us to pass this object from one object to another 
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is David</p>
+                <p>I am looking forwark for better projects</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in San Francisco</p>
+                            <p>My favourite language is JavaScript</p>
+                            <p>Besides Coding, I also love music and ramen</p>
+                            <button onClick={this.toggleDisplayBio}>Show Less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
