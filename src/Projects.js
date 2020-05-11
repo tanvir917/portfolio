688404d58b84dcdf69ef776ff43f66d2a51a86dd
tanvir <mdtanvirislams@gmail.com>
2020-05-12 05:28:46 +0600

    adding social profiles

--- src/Projects.js
+++ src/Projects.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+    render() {
+        console.log('this.props', this.props);
+        const { title, image, description, link } = this.props.project;
+        
+        return (
+            <div style={{display: 'inline-block', width: 300, margin: 10 }}>
+                <h3>{title}</h3>
+                <img src={image} alt='profile' style={{width:200, height:120}}/>
+                <p>{description}</p>
+                <a href={link}>{link}</a>
+            </div>
+        )
+    }
+}
+
 class Projects extends Component {
     render() {
         return (
-            <div>
+            <div >
                 <h2>Highlighted Projects</h2>
                 <div>
                     {
                         PROJECTS.map(PROJECT => {
                             return (
-                            <div key={PROJECT.id}>{PROJECT.title}</div>
+                                <Project key={PROJECT.id} project={PROJECT} />
                             );
                         })
                     }
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:mdtanvirislams@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/tanvir917',
+        image: githubIcon
+    },
+    {
+        id: 1,
+        link: 'https://www.linkedin.com/in/tanvir-islam-689b181a9/',
+        image: linkedinIcon
+    },
+    {
+        id: 1,
+        link: 'https://twitter.com/tanvir917',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
